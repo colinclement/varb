2a00d35e45397acdf1c8565ced4e06fb07c17f02
Colin Clement <colin.clement@gmail.com>
2016-02-07 00:08:19 -0500

    Fixed everything, works great now for higher than linear polynomials. added comments, using plain stochastic gradient descent with exponential moving average,

--- varb.py
+++ varb.py
@@ -1,26 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-N = 30
-P = 1
-x = np.linspace(0, 1, num=N)
-poly = [2., 1.] #m, b
+import autograd.numpy as np
+from autograd import grad
+
+N = 60
+x = np.linspace(-1.25, 1.25, num=N)
+poly = np.array([1., -0.5, -1., 0.5]) #m, b
 sigma = 0.3
 
 y_true = np.polyval(poly, x)
 d = y_true + np.random.normal(size = N, loc=0., scale=sigma)
-
-def sig(rho):
-    """
-    parametrize sigma to be non-negative
-    """
-    return np.log(1.+np.exp(rho))
-
-def rho(sig):
-    """
-    Inverse of sigma parametrization
-    """
-    return np.log(np.exp(np.abs(sig))-1.)
 
 def extract(lamb):
     """
@@ -34,7 +24,8 @@ def logposterior(poly, sigma, x, d):
     logposterior for polynomial model
     """
     N = len(x)
-    return (-np.sum((d - np.polyval(poly,x))**2/(2*sigma**2))
+    model = mypolyval(poly, x)
+    return (-np.sum((d[:, None] - model)**2/(2*sigma**2))
             - N/2*np.log(2*np.pi*sigma**2))
 
 def logq(poly, lamb):
@@ -46,53 +37,71 @@ def logq(poly, lamb):
     norm = -np.log(2*np.pi*sigs**2)/2.
     return np.sum((poly - mus)**2/(2*sigs**2) + norm)
 
-def grad_dkl(sigma, x, d, lamb, eps):
+def F_dF(lamb, sigma, x, d, eps):
     """
-    Stochastic gradient estimate of D_KL(q || p)
+    Variational lower bound and its gradient. I am using log
+    parameters for sigmas of variational model to enforce 
+    positivity.
+    input:
+        lamb: Array of lenght 2*N [mus, rhos=log(sigmas)]
+            mus and sigmas of variational model
+        sigma: noise estimate
+        x: array of length N, 
+            points at which polynomial is evaluated
+        d: array of length N, data points
+        eps: array of shape (batchsize, N)
+            Noise sample from trial distribution
+            standard normal sampling
+    output:
+        F, dF: variation free energy, its derivatives
     """
     mus, rhos = extract(lamb)
-    sigs = sig(rhos)
-    poly = mus + sigs * eps
-    thetas = np.array([np.polyval(p, x) for p in poly])
-    x_to_k = x[:,None] ** np.arange(poly.shape[1])[None, :]
-    dkl_mu = -((d - thetas).dot(x_to_k)/sigma**2).mean(0)
-    r = (poly - mus)/sigs
-    dkl_rho = ((eps*(r/sigs + dkl_mu) 
-               + (r**2 + 2)/sigs)/(1.+np.exp(rhos))).mean(0)
-    return np.r_[dkl_mu, dkl_rho]
+    N = len(mus)
+    sigs = np.exp(rhos)
+    thetas = mus[None, :] + sigs[None,:] * eps
+    model = np.array([np.polyval(t, x) for t in thetas])
+    r = d[None, :] - model
+    f = np.mean(np.sum(r**2, 1)/(2*sigma**2), 0) - rhos.sum()
+    df_mu = -(r.dot(np.array([x**(N-i-1) for i in range(N)]).T)/sigma**2)
+    df_rho = df_mu*eps*sigs - 1
+    return f, np.r_[df_mu.mean(0), df_rho.mean(0)]
 
-def varbayes(d, x, N, sigma = sigma, batchsize = 12, itnlim = 10,
-             rho = 0.95, eps = 1E-6, iprint = 0, tol = 1E-1):
+def varbayes(d, x, order, sigma = sigma, batchsize = 2000, itnlim = 50,
+             iprint = 0, lr = 0.001, alpha = 0.5, **kwargs):
     """
     Perform variational bayes to minimize D_KL(q || p) using
-    stochastic gradient descent and AdaDelta
-    """
-    lamb = np.random.randn(2*N)
-    e_grad2, e_delta2 = 0, 0
-    ll0 = logposterior(lamb[:N], sigma, x, d)
-    for i in range(itnlim):
-        eta = np.random.randn(batchsize, N)
-        d_dkl = grad_dkl(sigma, x, d, lamb, eta)
-        e_grad2 = rho * e_grad2 + (1-rho)*np.mean(d_dkl**2)
-        rms_egrad2 = np.sqrt(e_grad2 + eps)
-        rms_delta2 = np.sqrt(e_delta2 + eps)
-        delta = - rms_delta2/rms_egrad2 * d_dkl
-        lamb += delta
-        e_delta2 = rho * e_delta2 + (1-rho)*np.mean(delta**2)
-        ll1 = logposterior(lamb[:N], sigma, x, d)
-        d_ll = ll1 - ll0 
-        if np.abs(ll1 - ll0) < tol: 
-            print("LL changed by less than tol")
-            break
-        if d_ll < 0:
-            print("LL decreased")
-            eps /= 2
-            #break #Hack for stability
-        if iprint:
-            print("Itn {}: ll = {}".format(i, ll1))
-        ll0 = ll1
+    stochastic gradient descent
 
-    return np.r_[lamb[:N], sig(lamb[N:])]
+    inputs:
+        d: data (array of length N)
+        x: points polynomial is evaluated at (array of length N)
+        order: (int) order of polynomial
+        sigma : (float) estimate of noise variance
+        batchsize : number of samples to use to estimate gradient
+        itnlim: number of iterations to attempt SGD
+        iprint: (int) greater than 0 will print stuff
+        lr: SGD step stie
+        alpha: exponential moving average time constant
+    kwargs:
+        lamb0: initial mus and rhos=log(sigmas) for trial distn
+    
+    returns:
+        lamb: final trial distribution parameters
+    """
+    lamb = kwargs.get('lamb0', np.random.randn(2*order))
+    step = np.zeros_like(lamb)
+    F_av = 0 
+    for i in range(itnlim):
+        eta = np.random.randn(batchsize, order)
+        F, dF = F_dF(lamb, sigma, x, d, eta) 
+        #online exponential moving average
+        F_av += ((1-alpha)/(1-alpha**(i+1)))*(F - F_av)
+        step += ((1-alpha)/(1-alpha**(i+1)))*(lr*dF - step)
+        lamb -= step 
+        if iprint:
+            print("Itn {}: F = {}".format(i, F_av))
+
+    return np.r_[lamb[:order], np.exp(lamb[order:])]
 
 def plot_fit(lamb, x, d):
     m, s = extract(lamb)
@@ -122,4 +131,8 @@ def plot_fit_whisker(lamb, x, d, samples=1000, true_p = poly):
     plt.legend(loc='best')
     return f
 
+if __name__ == '__main__':
+    lamb = varbayes(d, x, 4, iprint=2)
+    plot_fit_whisker(lamb, x, d)
+    plt.show()
 
